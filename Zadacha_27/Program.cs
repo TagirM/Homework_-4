dea863def9d901938356bd5f139f2f1f5db66ed4
Admin <tmurzagaleev@yandex.ru>
2022-08-20 19:28:19 +0700

    Решение задачи 27 из ДЗ

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+//Создаем функцию GetSumNumbers
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Сумма цифр в числе равна " + GetSumNumber(number));
+
+
